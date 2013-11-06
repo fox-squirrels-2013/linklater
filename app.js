22f4805f9d54d617b36830c1a6ba2533edad1a59
amgando <sherif@amgando.org>
2013-11-06 01:10:39 -0800

    finished first pass at a link voting system.  css is still janky as hell

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.onload=function(){
+
   var firebaseUrl = 'https://linklater.firebaseio.com/'
   var linklater = new Firebase(firebaseUrl);
 
@@ -11,16 +12,26 @@ window.onload=function(){
   //
   ////////////////////////////////////////////////////////////////////
 
+  var linkList = {};
+
   function Link(snap){
     link          = snap.val();
+    this.name     = snap.name();
     this.title    = link.title;
     this.url      = link.url;
-    this.likes    = 1;
-    this.dislikes = 0;
+    this.likes    = link.likes;
+    this.dislikes = link.dislikes;
   };
 
   Link.prototype.html = function(){
-    markup = '<div class="linky"><a target="_new" href="'
+    markup = '<div class="linky">'
+           + '<div class="like" id="'
+           + this.name
+           + '">^</div>'
+           + '<div class="dislike" id="'
+           + this.name
+           + '">v</div>' 
+           + '<a target="_new" href="'
            + this.url 
            + '">'
            + this.title
@@ -41,9 +52,26 @@ window.onload=function(){
     return this.likes - this.dislikes;
   }
 
+  // got this from line 48 of https://www.firebase.com/tutorial/#session/hoz8ydoso7d
+  // and ... it doesn't work :(
+  // Link.prototype.id = function(){
+  //   return this.name.replace(/[^a-z0-9\-\_]/gi,'');
+  // }
+
   ////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////
 
+  $('#links').on('click', 'div.like', function(e){
+    link_id = e.target.id;
+    link = linkList[link_id];   
+    linklater.child(link_id).update({likes: link.likes += 1});
+  });
+
+  $('#links').on('click', 'div.dislike', function(e){
+    link_id = e.target.id;
+    link = linkList[link_id];
+    linklater.child(link_id).update({dislikes: link.dislikes += 1});
+  });
 
   // bind to the click function of submit and push new data
   $('#submit').click(function(e) {
@@ -55,6 +83,7 @@ window.onload=function(){
   // callback from firebase
   linklater.on('child_added', function(snapshot){
     var link = new Link(snapshot);
+    linkList[link.name] = link;
     $('#links').prepend(link.html());
   });
 };
